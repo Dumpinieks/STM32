66baac4e7d2c202ffc7b4eb1df06deaa2ffac537
Mikhail <tarassenko.m.v@gmail.com>
2018-02-24 11:48:11 +0200

    Updated gitignore, button-program
    CLICK = LIGHT

--- main.c
+++ main.c
@@ -1,24 +1,19 @@
 #include "stm32f1xx.h"
+#include "stm32f103xb.h"
 
-int main(void)
+int main()
 {
-    // Port C led setup
-	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
-	GPIOC->CRH |= GPIO_CRH_MODE13_1;
-	GPIOC->ODR ^= GPIO_ODR_ODR13;
-	while (1) {
-		GPIOC->ODR ^= GPIO_ODR_ODR13;
-		for (volatile int i = 0; i<300000; i++);
-		GPIOC->ODR ^= GPIO_ODR_ODR13;
-		for (volatile int i = 0; i<100000; i++);
-		GPIOC->ODR ^= GPIO_ODR_ODR13;
-		for (volatile int i = 0; i<100000; i++);
-		GPIOC->ODR ^= GPIO_ODR_ODR13;
-		for (volatile int i = 0; i<100000; i++);
-		GPIOC->ODR ^= GPIO_ODR_ODR13;
-		for (volatile int i = 0; i<100000; i++);
-		GPIOC->ODR ^= GPIO_ODR_ODR13;
-		for (volatile int i = 0; i<100000; i++);
-	}
+    RCC -> APB2ENR |= RCC_APB2ENR_IOPCEN;
+    RCC -> APB2ENR |= RCC_APB2ENR_IOPBEN;
+    GPIOC -> CRH |= GPIO_CRH_MODE13_1;
+    GPIOB -> CRL = (GPIOB -> CRL | GPIO_CRL_MODE1_1) & ~GPIO_CRL_MODE1_0;
+    GPIOB -> CRL &= ~GPIO_CRL_CNF1;
+    GPIOB -> ODR |= GPIO_ODR_ODR1;
+    while(1) {
+        if (GPIOB -> IDR & GPIO_IDR_IDR1)
+            GPIOC -> ODR |= GPIO_ODR_ODR13;
+        else
+            GPIOC -> ODR &= ~GPIO_ODR_ODR13;
+    }
     return 0;
 }
